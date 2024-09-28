50dbcf0f04502794c5584f2de9062cf8596f7ac1
lec <tgoding@163.com>
2024-09-28 21:58:57 +0800

    修正按钮样式

--- stserver/s.go
+++ stserver/s.go
@@ -2,7 +2,7 @@
  * @Description:
  * @Date: 2024-05-15 21:04:32
  * @LastEditors: lec
- * @LastEditTime: 2024-05-15 21:11:06
+ * @LastEditTime: 2024-09-22 22:59:08
  */
 package main
 
@@ -11,5 +11,6 @@ import "net/http"
 func main() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("../"))))
 	println("Server started on port 8080")
+	println("Visit http://localhost:8080 in your browser")
 	http.ListenAndServe(":8080", nil)
 }
