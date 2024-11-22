95746ef80a37afeb7157b886961702aa50c11b0a
lec <tgoding@163.com>
2024-11-22 16:39:30 +0800

    feat: 优化popmsg组件，避免重复调用导致的多次显示问题
    - 在popmsg组件中添加防重复调用机制
    - 确保同一时间只显示一个消息提示
    - 提升用户体验，避免消息堆叠显示

--- js/popmsg.js
+++ js/popmsg.js
@@ -1,154 +1,138 @@
-var popmsgStru = {
+var popmsg = function(){
 
 
-    position_value: null,
-    type_value: null,
-    time_value: null,
-    mask_value: null,
-    msg_value: null,
-    size_value: null,
 
-    init: function () {
-         var position_value="top-center";//top-center:上中,top-left:上左,top-right:上右,center-center:中中,center-left:中左,center-right:中右,bottom-center:下中,bottom-left:下左,bottom-right:下右
-        var type_value= "com-msg";//com-msg:普通消息,info-msg:信息消息,success-msg:成功消息,warning-msg:警告消息,error-msg:错误消息
-        var time_value= 3000;
-        var mask_value= false;
-        var msg_value= "提示信息";
-        var size_value="m";//m:medium,s:small,l:large
-        return this;
-    },
+return (function () {
+    var position_value = "top-center"; // 默认位置
+    var type_value = "com-msg"; // 默认类型
+    var time_value = 3000; // 默认时间
+    var mask_value = false; // 默认不显示遮罩
+    var msg_value = "提示信息"; // 默认消息
+    var size_value = "m"; // 默认大小
 
-    
-   
+    var init = function () {
+        // 重置所有参数为默认值
+        position_value = "top-center";
+        type_value = "com-msg";
+        time_value = 3000;
+        mask_value = false;
+        msg_value = "提示信息";
+        size_value = "m";
+        return this;
+    };
 
-   
-    //设置位置
-    pos: function (value="top-center") {
-        this.position_value = value;
-        return this;
-    },
-    //设置类型
-    type: function (value="com-msg") {
-        this.type_value = value;
-        return this;
-    },
-    //设置时间
-    time: function (value=3000) {
-        this.time_value = value;
-        return this;
-    },
-    //设置遮罩层
-    mask: function (value=false) {
-        this.mask_value = value;
-        return this;
-    },
-    //设置信息
-    body: function (value="提示信息") {
-        this.msg_value = value;
-        return this;
-    },
-    //设置大小
-    size: function (value="m") {
-        this.size_value = value;
-        if(value=="l"){
-            this.size_value="large-msg";
-        }else if(value=="m"){
-            this.size_value="medium-msg";
-        }else if(value=="s"){
-            this.size_value="small-msg";
-        }
-        return this;
-    },
-    //构造弹出消息盒子，承载消息，以便在同一位置显示多个消息可以依次显示
-    createboxs: function () {
+    var createboxs = function () {
         console.log('popmsg createboxs');
         var box = document.createElement('div');
         box.className = 'popmsg-box';
-        box.classList.add(this.position_value);//设置消息盒子位置
+        box.classList.add(position_value); // 设置消息盒子位置
         document.body.appendChild(box);
         return box;
-    },
-    //如果消息盒子内部元素为空，则删除消息盒子
-    removebox: function (box) {
+    };
+
+    var removebox = function (box) {
         console.log(box);
         if (box.children.length == 0) {
             box.remove();
         }
-    },
-    createMsg: function () {
-        var _this = this;
+    };
+
+    var createMsg = function () {
         console.log('popmsg createMsg');
         var msg = document.createElement('div');
         msg.className = 'popmsg-msg';
-        msg.innerHTML = this.msg_value;
-        msg.classList.add(this.type_value);//设置消息类型
-        msg.classList.add(this.size_value);//设置消息大小
-        //设置消息显示时间
+        msg.innerHTML = msg_value;
+        msg.classList.add(type_value); // 设置消息类型
+        msg.classList.add(size_value); // 设置消息大小
         setTimeout(function () {
-            _this.removeMsg(msg);
-        }, _this.time_value);
+            removeMsg(msg);
+        }, time_value);
         return msg;
-    },
-    //删除消息
-    removeMsg: function (msg) {
-        var msgParentElement=msg.parentElement;
+    };
+
+    var removeMsg = function (msg) {
+        var msgParentElement = msg.parentElement;
         msg.remove();
-        //删除遮罩层
-        this.removeMask();
-        console.log(msg);    
-        this.removebox(msgParentElement);
-    },
-    //构建遮罩层
-    createMask: function () {
-        var _this = this;
+        removeMask();
+        console.log(msg);
+        removebox(msgParentElement);
+    };
+
+    var createMask = function () {
         console.log('popmsg createMask');
-        //检查是否存在遮罩层，如果存在，则不创建遮罩层
         var mask = document.querySelector('.popmsg-mask');
         if (mask) {
             console.log('popmsg 遮罩层已存在');
             return mask;
         }
-        var mask = document.createElement('div');
+        mask = document.createElement('div');
         mask.className = 'popmsg-mask';
         document.body.appendChild(mask);
-        //点击关闭遮罩层
-        mask.addEventListener('click', function () {
-            _this.removeMask();
-        });
+        mask.addEventListener('click', removeMask);
         return mask;
-    },
-    //删除遮罩层，检查是否有关联遮罩层的消息弹框，如果有就不删除遮罩层。
-    removeMask: function () {
+    };
+
+    var removeMask = function () {
         console.log('popmsg removeMask');
-        var popmsg_mask_has = document.querySelector('.popmsg-mask-has');//检查是否存在关联遮罩层的消息弹框
+        var popmsg_mask_has = document.querySelector('.popmsg-mask-has');
         if (!popmsg_mask_has) {
             var mask = document.querySelector('.popmsg-mask');
-            if (mask) { 
+            if (mask) {
                 mask.remove();
             }
-        }else{
+        } else {
             console.log('popmsg 有关联遮罩层的消息弹框，不删除遮罩层');
         }
-    },
-     //显示弹框
-     msg: function () {
-        // 显示弹框
+    };
+
+    var msg = function () {
         console.log('popmsg p');
-        //检查是否存在position_value的消息盒子
-        var box = document.querySelector('.' + this.position_value);
+        var box = document.querySelector('.' + position_value);
         if (!box) {
-            box = this.createboxs();
+            box = createboxs();
         }
-        var msg = this.createMsg();
-        box.appendChild(msg);
-        //检查是否启用遮罩层
-        if (this.mask_value) {
-            var mask = this.createMask();
-            msg.classList.add('popmsg-mask-has');//标识消息弹框关联了遮罩层
+        var message = createMsg();
+        box.appendChild(message);
+        if (mask_value) {
+            createMask();
+            message.classList.add('popmsg-mask-has');
         }
-    }
+    };
 
-
+    return {
+        init: init,
+        pos: function (value = "top-center") {
+            position_value = value;
+            return this;
+        },
+        type: function (value = "com-msg") {
+            type_value = value;
+            return this;
+        },
+        time: function (value = 3000) {
+            time_value = value;
+            return this;
+        },
+        mask: function (value = false) {
+            mask_value = value;
+            return this;
+        },
+        body: function (value = "提示信息") {
+            msg_value = value;
+            return this;
+        },
+        size: function (value = "m") {
+            size_value = value;
+            if (value == "l") {
+                size_value = "large-msg";
+            } else if (value == "m") {
+                size_value = "medium-msg";
+            } else if (value == "s") {
+                size_value = "small-msg";
+            }
+            return this;
+        },
+        msg: msg
+    };
+})();
 }
-
-var popmsg=popmsgStru.init()
