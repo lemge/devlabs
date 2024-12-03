16589e7824a727b16da5f4a90ee60b7893a7a4ed
lec <tgoding@163.com>
2024-12-03 17:23:56 +0800

    导航栏组件优化： 1. 添加响应式布局支持 2. 实现导航项自动折叠功能 3. 新增展开/折叠按钮 4. 优化暗色主题样式

--- js/ajaxnavi.js
+++ js/ajaxnavi.js
@@ -3,6 +3,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
     naviElements.forEach(naviElement => {
         const dataSource = naviElement.getAttribute('data-navi-source');
+        //构造折叠展开按钮并添加到naviElement中
+        var foldButton=document.createElement('button');
+        foldButton.classList.add('dl-navi-fold');
+        foldButton.textContent='≡';
+        foldButton.setAttribute('title','展开');
+        naviElement.appendChild(foldButton);
+        foldButton.addEventListener('click', function(e) {
+            var btntitle=e.target.getAttribute('title');
+            console.log(naviElement,"naviElement");
+            var navList=naviElement.querySelector('.dl-navi-list');
+            
+            if (btntitle=='展开') {
+                //执行展开操作
+                navList.style.height='auto';
+                e.target.textContent='-';
+                e.target.setAttribute('title','折叠');
+            } else {
+                //执行折叠操作
+                resizeNavi(naviElement);
+                e.target.textContent='≡';
+                e.target.setAttribute('title','展开');
+            }
+            
+        });
         
         if (dataSource) {
             fetch(dataSource)
@@ -38,10 +62,35 @@ document.addEventListener('DOMContentLoaded', function() {
                     });
 
                     naviElement.appendChild(navList);
+                    resizeNavi(naviElement);
+                    //当appendChild完成时，调整尺寸
                 })
                 .catch(error => console.error('Error fetching navigation data:', error));
         }
     });
 });
+
+
+
+
+//调整尺寸，当dl-navi-list的高度大于dl-navi-item的高度时，调整dl-navi-list的高度为dl-navi-item的高度，并显示折叠展开按钮
+var resizeNavi = function(naviElement) {
+    console.log('resizeNavi');
+    var navList = naviElement.querySelector('.dl-navi-list');
+    if (navList) { // 检查navList是否存在
+        var navListHeight = navList.scrollHeight;
+        var naviListItem = navList.querySelector('.dl-navi-item');
+        if (naviListItem) { // 检查naviListItem是否存在
+            var naviListItemHeight = naviListItem.clientHeight;
+            if (navListHeight > naviListItemHeight) {
+                navList.style.height = naviListItemHeight + 'px';
+                //显示折叠展开按钮
+                naviElement.querySelector('.dl-navi-fold').style.display = 'block';
+                naviElement.style.paddingRight = '30px';
+                
+            }
+        }
+    }
+}
 
 
